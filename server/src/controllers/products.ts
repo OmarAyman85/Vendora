2c8fdec495b743512cf2ce6acfc10575c7104c0b
OmarAyman85 <oamr.shivy85@gmail.com>
2024-12-31 03:55:29 +0200

    Product routes and controllers

--- server/src/controllers/products.ts
+++ server/src/controllers/products.ts
@@ -1,11 +1,17 @@
 import { Request, Response } from "express";
 import { prismaCLient } from "..";
 import { createProductSchema } from "../schema/products";
-
+import { NotFoundException } from "../exceptions/NotFound";
+import { ErrorCode } from "../exceptions/root";
+//-----------------------------------------------------------
 export const createProduct = async (req: Request, res: Response) => {
   createProductSchema.parse(req.body);
   console.log("I am here and the req body is 1: ", req.body);
+  //----------------------------------------------------------------------
+  //----------------------------------------------------------------------
   delete req.body.user;
+  //----------------------------------------------------------------------
+  //----------------------------------------------------------------------
   const product = await prismaCLient.product.create({
     data: {
       ...req.body,
@@ -15,3 +21,79 @@ export const createProduct = async (req: Request, res: Response) => {
   console.log("I am here and the req body is : ", req.body);
   res.json(product);
 };
+//-----------------------------------------------------------
+export const updateProduct = async (req: Request, res: Response) => {
+  try {
+    //----------------------------------------------------------------------
+    //----------------------------------------------------------------------
+    delete req.body.user;
+    //----------------------------------------------------------------------
+    //----------------------------------------------------------------------
+    const product = req.body;
+    if (product.tags) {
+      product.tags = product.tags.join(",");
+    }
+    const updatedProduct = await prismaCLient.product.update({
+      where: {
+        id: +req.params.id,
+      },
+      data: product,
+    });
+    res.status(201).json(updatedProduct);
+  } catch (err) {
+    throw new NotFoundException(
+      "Product is not found !!",
+      ErrorCode.PRODUCT_NOT_FOUND
+    );
+  }
+};
+//-----------------------------------------------------------
+export const deleteProduct = async (req: Request, res: Response) => {
+  try {
+    //----------------------------------------------------------------------
+    //----------------------------------------------------------------------
+    delete req.body.user;
+    //----------------------------------------------------------------------
+    //----------------------------------------------------------------------
+    await prismaCLient.product.delete({
+      where: {
+        id: +req.params.id,
+      },
+    });
+    res.status(204);
+  } catch (err) {
+    throw new NotFoundException(
+      "Product is not found !!",
+      ErrorCode.PRODUCT_NOT_FOUND
+    );
+  }
+};
+//-----------------------------------------------------------
+export const listProducts = async (req: Request, res: Response) => {
+  const count = await prismaCLient.product.count(); // for pagination
+  const products = await prismaCLient.product.findMany({
+    skip: +req.query.skip! || 0,
+    take: 5,
+  });
+  res.json({
+    count,
+    data: products,
+  });
+};
+//-----------------------------------------------------------
+export const getProductById = async (req: Request, res: Response) => {
+  try {
+    const product = await prismaCLient.product.findFirstOrThrow({
+      where: {
+        id: +req.params.id,
+      },
+    });
+    res.json(product);
+  } catch (err) {
+    throw new NotFoundException(
+      "Product is not found !!",
+      ErrorCode.PRODUCT_NOT_FOUND
+    );
+  }
+};
+//-----------------------------------------------------------
